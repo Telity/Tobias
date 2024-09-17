66ab6934bf72b18de8b2f440ec68b671c7821490
Telity <Nigu0001@Stud.kea.dk>
2024-09-17 10:12:45 +0200

    Hello again tobias

---
+++ src/main/java/gruppe7/tobias/TobiasApplication.java
@@ -0,0 +1,13 @@
+package gruppe7.tobias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gruppe7/tobias/TobiasApplicationTests.java
@@ -0,0 +1,13 @@
+package gruppe7.tobias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
